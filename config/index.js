ac16e34c87d10e417fbedfd3d231368fdd306b77
Brady Bovero <bbbove20@gmail.com>
2021-02-15 14:36:17 -0700

    underconstruction.

--- config/index.js
+++ config/index.js
@@ -14,4 +14,7 @@ export const UNDER_CONSTRUCTION = process.env.NEXT_PUBLIC_UNDER_CONSTRUCTION;
 
 export const OPTION_LABELS = { size: "Title", quantity: "Quantity" };
 
-// 2021-2-15: set under constructions : homepage adjust so doesnt overlap. Testing Home page.
+/* 2021-2-15: set under constructions : homepage adjust so doesnt overlap. 
+    Testing Home page.
+    still needs fixed.
+ */

4405dca1cceced0f8bded1587e6486fe2a1967f2
Brady Bovero <bbbove20@gmail.com>
2021-02-15 14:07:07 -0700

    view prod.

--- config/index.js
+++ config/index.js
@@ -13,3 +13,5 @@ export const SHOPIFY_DOMAIN = prod
 export const UNDER_CONSTRUCTION = process.env.NEXT_PUBLIC_UNDER_CONSTRUCTION;
 
 export const OPTION_LABELS = { size: "Title", quantity: "Quantity" };
+
+// 2021-2-15: view production.
